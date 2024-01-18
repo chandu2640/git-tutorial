ed176c8bab7ba8937890fd4d5ff4a2f43b21cf05
chandancine <chandan.cinepebble@gmail.com>
2024-01-19 01:40:32 +0530

    fsdfsd

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-let a =5
+
+let btn = document.getElementById("btn")
+
+btn.addEventListener("click", clickBtn)
+
+function clickBtn(){
+    let num1 = document.getElementById("num1").value
+let num2 = document.getElementById("num2").value
+document.getElementById("ans").innerText = parseInt(num1) + parseInt(num2)
+   console.log(num1, num2)
+}
